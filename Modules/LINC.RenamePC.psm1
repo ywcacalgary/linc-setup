a5d18901ea8b8f7115e4148f252e92d30dacf1d5
ywcalgary <nchikhlia@ywcalgary.ca>
2026-06-24 13:02:55 -0600

    Moved automatic restart from RenamePC to Setup

--- Modules/LINC.RenamePC.psm1
+++ Modules/LINC.RenamePC.psm1
@@ -3,7 +3,7 @@ $ErrorActionPreference = 'Stop'
 
 function Rename-LincPC {
     $NewName = (Get-Content -Path ".\ComputerName.txt").Trim()
-    Rename-Computer -NewName $NewName -Force -Restart
+    Rename-Computer -NewName $NewName -Force
 }
 
 Export-ModuleMember -Function Rename-LincPC
